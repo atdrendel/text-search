d9039ed90eacf4adb54b9a652c6222e233e7da53
Anthony Drendel <me@anthonydrendel.com>
2019-12-08 18:57:06 +0100

    Add test to verify bindings

--- text-search/src/lib.rs
+++ text-search/src/lib.rs
@@ -2,8 +2,19 @@ use text_search_sys;
 
 #[cfg(test)]
 mod tests {
+  use super::*;
+
   #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
+  fn can_use_countedset() {
+    let count = unsafe {
+      let set = text_search_sys::tsearch_countedset_init();
+      text_search_sys::tsearch_countedset_add_int(set, 1);
+      text_search_sys::tsearch_countedset_add_int(set, 1);
+      text_search_sys::tsearch_countedset_add_int(set, 2);
+      let count = text_search_sys::tsearch_countedset_get_count(set);
+      text_search_sys::tsearch_countedset_free(set);
+      count
+    };
+    assert_eq!(2, count);
   }
 }
