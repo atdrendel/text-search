be3dbee7c3db207bbb392bd8302d22025859d340
Anthony Drendel <me@anthonydrendel.com>
2019-12-14 08:38:28 +0100

    Remove unnecessary test in lib.rs

--- text-search/src/lib.rs
+++ text-search/src/lib.rs
@@ -1,22 +1,5 @@
 pub mod counted_set;
 
-use text_search_sys;
-
 #[cfg(test)]
 mod tests {
-  use super::*;
-
-  #[test]
-  fn can_use_countedset() {
-    let count = unsafe {
-      let set = text_search_sys::tsearch_countedset_init();
-      text_search_sys::tsearch_countedset_add_int(set, 1);
-      text_search_sys::tsearch_countedset_add_int(set, 1);
-      text_search_sys::tsearch_countedset_add_int(set, 2);
-      let count = text_search_sys::tsearch_countedset_get_count(set);
-      text_search_sys::tsearch_countedset_free(set);
-      count
-    };
-    assert_eq!(2, count);
-  }
 }
